a633f32f39282782f24b66b62644b8620563f950
David Hoyt <xss@xss.cx>
2022-02-25 09:02:47 -0500

    Update build-ubsan.sh

--- srd_tools-24.100.3/example-cryptex/build-ubsan.sh
+++ srd_tools-24.100.3/example-cryptex/build-ubsan.sh
@@ -63,10 +63,8 @@ git diff /private/tmp/src-libclang_rt.ubsan_ios_dynamic.dylib.xml /private/tmp/d
 echo "End of entitlement checks....."
 echo "Delete srd-universal-cryptex.dmg"
 rm srd-universal-cryptex.dmg
-# Removing this --force which is just for experimentation, this is meant to generate AMFI Noise.....
-# echo "FIXUP for ubsan dylib in attempt to silence AMFI, the entitlements aren't making it thru all the time in Makefile so this line is the FIXUP and TODO........"
-# codesign --force -s - --entitlements src/hello/entitlements.plist com.example.cryptex.dstroot/usr/bin/hello
-# 
+echo "FIXUP for ubsan dylib in attempt to silence AMFI based on the suggestion in PR42 and examples in PR48 + PR49"
+codesign --force -s - --entitlements src/hello/entitlements.plist com.example.cryptex.dstroot/usr/bin/hello 
 echo "\n Must check this entitlement visually... codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/hello that attempts to setup use of libclang_rt.ubsan_ios_dynamic.dylib\n"
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/hello
 echo "\n\n Now making the SRD Example UBSAN DMG containing Frida, debugserver, toyboxunstripped and the example hello linked to ubsan.dylib..\n"
