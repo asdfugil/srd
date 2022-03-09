cd748bcb5473752d52b0040e7a0a52fa26be2d6e
David Hoyt <xss@xss.cx>
2022-03-09 17:13:30 -0500

    wget https://github.com/xsscx/srd/raw/main/dmg/srd-universal-cryptex.dmg
    wget https://github.com/xsscx/srd/raw/main/dmg/srd-universal-cryptex.dmg

--- dmg/install.sh
+++ dmg/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://xss.cx/srd/dmg/srd-universal-cryptex.dmg
+wget https://github.com/xsscx/srd/raw/main/dmg/srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
 cryptexctl uninstall com.example.cryptex
