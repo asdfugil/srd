e3cb618818af47a0d1cb42bd5ec2872e0fd8bfed
David Hoyt <xss@xss.cx>
2022-03-08 21:53:12 -0500

    uri fixup
    uri fixup

--- srd_tools-24.100.3/example-cryptex/srd-cryptex-file-attribute-collector.sh
+++ srd_tools-24.100.3/example-cryptex/srd-cryptex-file-attribute-collector.sh
@@ -5,43 +5,43 @@ echo "=========================================="
 echo "Collecting Info"
 date > srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/debugserver >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/frida >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/frida >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/frida >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/frida >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/frida >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/frida >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/frida >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/nvram >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/simple-server >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
-codesign -dvvv /Users/xss/example-cryptex/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
+codesign -dvvv /Users/xss/example-cryptex/src/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
 otool -L  com.example.cryptex.dstroot/usr/bin/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/toybox >> srd-cryptex-file-attribute-collector.log 2>&1
