e2ef9529a261cec2cda7c00084c1b037a8ce32f3
David Hoyt <xss@xss.cx>
2022-03-12 09:41:36 -0500

    Update srd-cryptex-file-attribute-collector.sh

--- srd_tools-24.100.3/example-cryptex/srd-cryptex-file-attribute-collector.sh
+++ srd_tools-24.100.3/example-cryptex/srd-cryptex-file-attribute-collector.sh
@@ -3,7 +3,8 @@ echo "=========================================="
 echo "SRD Cryptex File Profile Collector Start"
 echo "=========================================="
 echo "Collecting Info"
-date > srd-cryptex-file-attribute-collector.log 2>&1
+echo "==========================================" > srd-cryptex-file-attribute-collector.log 2>&1
+date >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 codesign -dvvv /Users/xss/example-cryptex/src/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
 hexdump -n 4 com.example.cryptex.dstroot/usr/bin/cryptex-run >> srd-cryptex-file-attribute-collector.log 2>&1
@@ -52,8 +53,15 @@ hexdump -n 4 com.example.cryptex.dstroot/usr/bin/hello >> srd-cryptex-file-attri
 otool -L  com.example.cryptex.dstroot/usr/bin/hello >> srd-cryptex-file-attribute-collector.log 2>&1
 xcrun dyldinfo -rebase -bind  com.example.cryptex.dstroot/usr/bin/hello >> srd-cryptex-file-attribute-collector.log 2>&1
 machodump -i com.example.cryptex.dstroot/usr/bin/hello >> srd-cryptex-file-attribute-collector.log 2>&1
-
+nm -a com.example.cryptex.dstroot/usr/bin/cryptex-run	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/debugserver	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/dropbear >> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/frida	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/hello	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/nvram	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/simple-server	>> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/simple-shell >> srd-cryptex-file-nm-collector.log 2>&1
+nm -a com.example.cryptex.dstroot/usr/bin/toybox >> srd-cryptex-file-nm-collector.log 2>&1
 echo "=========================================="
 echo "SRD Cryptex File Profile Collector Stop"
 echo "=========================================="
-cat srd-cryptex-file-attribute-collector.log
